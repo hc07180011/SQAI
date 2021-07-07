3c78e5e3275fcd9818b2d68c89238fc21c77574e
henrychao <>
2021-07-07 17:04:58 +0800

    change app location

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+az staticwebapp create \
+    -n sqai-2021 \
+    -g sqai \
+    -s https://github.com/hc07180011/SQAI \
+    -l EastAsia \
+    -b master \
+    --token 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
